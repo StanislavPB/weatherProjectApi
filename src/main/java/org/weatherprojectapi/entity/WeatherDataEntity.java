0c848790f208659fb28eda89536dc588230f7097
StanislavPB <perminovsb@gmail.com>
2024-04-15 18:31:00 +0100

    init

--- src/main/java/org/weatherprojectapi/entity/WeatherDataEntity.java
+++ src/main/java/org/weatherprojectapi/entity/WeatherDataEntity.java
@@ -1,13 +1,10 @@
 package org.weatherprojectapi.entity;
 
-import javax.persistence.*;
+
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 
 @Entity
 @Data
